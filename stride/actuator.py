02f85bc69373d06cbdbee22a26fbea61c0f93688
Vojtech Kalivoda <kali.voitech@gmail.com>
2025-08-18 12:32:11 +0200

    clean-up

--- stride/actuator.py
+++ stride/actuator.py
@@ -1,55 +1,15 @@
-import time
-import can
 import math
 import struct
-
-from enum import IntEnum, auto
-from typing import Iterable
-from dataclasses import dataclass
+import time
 from collections.abc import Buffer
+from dataclasses import dataclass
+from enum import IntEnum
+
+from stride.communication import CommunicationType, Parameter, RobstrideBus
 
 
-CAN_PORT = "can0"
-ZERO_DATA = [0, 0, 0, 0, 0, 0, 0, 0]
-
-
-class CommunicationType(IntEnum):
-    DeviceID = 0
-    Control = 1
-    Feedback = 2
-    Enable = 3
-    Disable = 4
-    MechanicalZero = 6
-    ReadParameter = 17
-    WriteParameter = 18
-    FaultFeedback = 21
-
-
-class Parameter(IntEnum):
-    RunMode = 0x7005
-    IqRef = 0x7006
-    SpdRef = 0x700A
-    LimitTorque = 0x700B
-    CurKp = 0x7010
-    CurKi = 0x7011
-    CurFiltGain = 0x7014
-    LocRef = 0x7016
-    LimitSpd = 0x7017
-    LimitCur = 0x7018
-    MechPos = 0x7019
-    IqFilt = 0x701A
-    MechVel = 0x701B
-    VBus = 0x701C
-    LocKp = 0x701E
-    SpdKp = 0x701F
-    SpdKi = 0x7020
-    SpdFiltGain = 0x7021
-    AccRad = 0x7022
-    VelMax = 0x7024
-    AccSet = 0x7025
-    EPScanTime = 0x7026
-    CanTimeout = 0x7028
-    ZeroSta = 0x7029
+class ActuatorLimitException(ValueError):
+    pass
 
 
 class MotorMode(IntEnum):
@@ -82,49 +42,32 @@ class Feedback():
     temp: float
 
 
-class RobstrideBus:
-    def __init__(self):
-        self.can_bus = can.interface.Bus(interface='socketcan', channel=CAN_PORT)
-
-    def recv(self):
-        res = self.can_bus.recv()
-        if not res or res.is_error_frame:
-            Exception("response error")
-        return res
-
-    def send(self, comm_type: CommunicationType, id_field: int, data: bytes | bytearray | int | Iterable[int] | None = ZERO_DATA):
-        arb_id = id_field + (comm_type << 24)
-        msg = can.Message(arbitration_id=arb_id, data=data, is_extended_id=True)
-        self.can_bus.send(msg)
-        # print([msg])
-
-
 class Actuator:
+    ANGLE_LIMIT = 4 * math.pi
+    VELOCITY_LIMIT = 44
+    TORQUE_LIMIT = 17
+
     def __init__(self, motor_id: int, host_id: int = 0xAA):
         self.bus = RobstrideBus()
         self.motor_id = motor_id
         self.host_id = host_id
         self.id_field = self.motor_id + (self.host_id << 8)
 
-    def get_feedback(self):
+    def get_feedback(self) -> Feedback:
         def _unpack(x: Buffer) -> int:
             return struct.unpack('>H', x)[0]
 
-        def _unpack_range(x: Buffer, r: int) -> float:
+        def _unpack_range(x: Buffer, r: int | float) -> float:
             return _unpack(x) / 65535 * r - r / 2
 
         def _get_error_bit(error_bits: int, i: int) -> bool:
             return bool(error_bits & (1 << i))
 
-        angle_range = 8 * math.pi
-        vel_range = 88
-        torque_range = 34
-
         res = self.bus.recv()
 
-        angle = _unpack_range(res.data[0:2], angle_range)
-        velocity = _unpack_range(res.data[2:4], vel_range)
-        torque = _unpack_range(res.data[4:6], torque_range)
+        angle = _unpack_range(res.data[0:2], Actuator.ANGLE_LIMIT * 2)
+        velocity = _unpack_range(res.data[2:4], Actuator.VELOCITY_LIMIT * 2)
+        torque = _unpack_range(res.data[4:6], Actuator.TORQUE_LIMIT * 2)
 
         temp = _unpack(res.data[6:8]) / 10
 
@@ -145,26 +88,17 @@ class Actuator:
             undervoltage=_get_error_bit(error_bits, 0)
         )
 
-        feedback = Feedback(1, status, angle, velocity, torque, temp)
-        print(feedback)
+        return Feedback(1, status, angle, velocity, torque, temp)
 
-        return feedback
-
-    def enable(self):
+    def enable(self) -> Feedback:
         self.bus.send(CommunicationType.Enable, self.id_field)
         return self.get_feedback()
 
-    def disable(self):
+    def disable(self) -> Feedback:
         self.bus.send(CommunicationType.Disable, self.id_field)
         return self.get_feedback()
 
-    def write_param(self, pid: Parameter, value: int | float):
-        """
-        data:
-        - 0~1: index
-        - 2~3: 00
-        - 4~7: data (little-endian)
-        """
+    def write_param(self, pid: Parameter, value: int | float) -> Feedback:
         pid_bytes = struct.pack('<I', pid)[:2]
 
         if pid in [0x7005, 0x7026, 0x7028, 0x7029]:
@@ -186,18 +120,23 @@ class Actuator:
         res = self.bus.recv()
         return struct.unpack('<f', res.data[4:])[0]
 
-    def get_device_info(self):
-        self.bus.send(CommunicationType.DeviceID, self.id_field, ZERO_DATA)
+    def get_device_info(self) -> int:
+        self.bus.send(CommunicationType.DeviceID, self.id_field)
         res = self.bus.recv()
         return struct.unpack('<Q', res.data)[0]
 
-    def set_mechanical_zero(self):
+    def set_mechanical_zero(self) -> Feedback:
         self.bus.send(CommunicationType.MechanicalZero, self.id_field, [1, 0, 0, 0, 0, 0, 0, 0])
         return self.get_feedback()
 
-    def command(self, torque):
-        id_field = self.motor_id + (torque << 8)
-        self.bus.send(CommunicationType.Control, id_field, ZERO_DATA)
+    def torque_command(self, torque: float) -> Feedback:
+        if abs(torque) > Actuator.TORQUE_LIMIT:
+            raise ActuatorLimitException("Torque command is over actuator limit")
+
+        norm_torque = int((torque + Actuator.TORQUE_LIMIT) / (2 * Actuator.TORQUE_LIMIT) * 65535)
+        id_field = self.motor_id + (norm_torque << 8)
+
+        self.bus.send(CommunicationType.Control, id_field)
         return self.get_feedback()
 
     def shutdown(self):
@@ -207,27 +146,29 @@ class Actuator:
 
 def main():
     a.enable()
+    a.write_param(Parameter.RunMode, 1)  # put motor into operational mode
+    a.set_mechanical_zero()
+    a.write_param(Parameter.LocKp, 40)
+    a.write_param(Parameter.SpdKp, 6)
+    a.write_param(Parameter.SpdKi, 0.02)
+    a.write_param(Parameter.LimitSpd, 44)
 
-    a.write_param(Parameter.RunMode, 1)
-    a.write_param(Parameter.LimitSpd, 50.0)
-    a.write_param(Parameter.LocRef, 0)
+    a.write_param(Parameter.LocRef, 0.6)
+
+    # a.torque_command(1.0) # apply 1Nm torque
+
     time.sleep(2)
+    # a.torque_command(-1.0) # apply 1Nm torque in counter-clockwise
+    # time.sleep(1)
+    q = a.read_param(Parameter.MechPos)
+    print(q)
 
-    for _ in range(2):
-        a.write_param(Parameter.LocRef, -1)
-        time.sleep(2)
-        a.write_param(Parameter.LocRef, 1)
-        time.sleep(2)
-
-    a.write_param(Parameter.LocRef, 0.0)
-    time.sleep(2)
     a.shutdown()
 
 
-try:
-    a = Actuator(127)
-    main()
-except Exception as e:
-    print(e)
-except KeyboardInterrupt:
-    a.shutdown()
+if __name__ == "__main__":
+    try:
+        a = Actuator(127)
+        main()
+    except KeyboardInterrupt:
+        a.shutdown()
---
+++ stride/communication.py
@@ -0,0 +1,62 @@
+from enum import IntEnum
+from typing import Iterable
+
+import can
+
+CAN_PORT = "can0"
+ZERO_DATA = [0, 0, 0, 0, 0, 0, 0, 0]
+
+
+class CommunicationType(IntEnum):
+    DeviceID = 0
+    Control = 1
+    Feedback = 2
+    Enable = 3
+    Disable = 4
+    MechanicalZero = 6
+    ReadParameter = 17
+    WriteParameter = 18
+    FaultFeedback = 21
+
+
+class Parameter(IntEnum):
+    RunMode = 0x7005
+    IqRef = 0x7006
+    SpdRef = 0x700A
+    LimitTorque = 0x700B
+    CurKp = 0x7010
+    CurKi = 0x7011
+    CurFiltGain = 0x7014
+    LocRef = 0x7016
+    LimitSpd = 0x7017
+    LimitCur = 0x7018
+    MechPos = 0x7019
+    IqFilt = 0x701A
+    MechVel = 0x701B
+    VBus = 0x701C
+    LocKp = 0x701E
+    SpdKp = 0x701F
+    SpdKi = 0x7020
+    SpdFiltGain = 0x7021
+    AccRad = 0x7022
+    VelMax = 0x7024
+    AccSet = 0x7025
+    EPScanTime = 0x7026
+    CanTimeout = 0x7028
+    ZeroSta = 0x7029
+
+
+class RobstrideBus:
+    def __init__(self):
+        self.can_bus = can.interface.Bus(interface='socketcan', channel=CAN_PORT)
+
+    def recv(self):
+        res = self.can_bus.recv()
+        if not res or res.is_error_frame:
+            Exception("response error")
+        return res
+
+    def send(self, comm_type: CommunicationType, id_field: int, data: bytes | bytearray | int | Iterable[int] | None = ZERO_DATA):
+        arb_id = id_field + (comm_type << 24)
+        msg = can.Message(arbitration_id=arb_id, data=data, is_extended_id=True)
+        self.can_bus.send(msg)
